84189ab05889c1acda645577080c0ed5b0ab0555
Biltu Maji <getbiltu@gmail.com>
2018-06-08 17:14:13 +0530

    added count view per step

--- cow-bull.py
+++ cow-bull.py
@@ -1,15 +1,18 @@
-prenum=[1,2,9,4]
-uin1=input("Enter ur guess:")
+import random as rnd
+random=rnd.randint(1000,9999)
+guess=[]
+guess=str(random)
+
+uin=input("Enter ur guess Of 4 digit:")
 uin1=[]
 uin1=uin
 
-print(uin1[2])
 bull=0
 cow=0
 for i in range(0,4):
-    if (int(uin1[i])!=prenum[i]):
+    if(int(uin1[i])!=int(guess[i])):
         bull=bull+1
-    elif(int(uin1[i])==prenum[i]):
+    elif(int(uin1[i])==int(guess[i])):
         cow=cow+1
 
-print("Bull=",bull,"cow=",cow)
+print("Bull=",bull,"cow=",cow,"\nand Number Is:",random)
